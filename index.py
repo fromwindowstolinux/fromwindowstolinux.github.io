e2e059fc81b0f3ed379320adb1ed26bac9260b71
fromwindowstolinux <43229477+fromwindowstolinux@users.noreply.github.com>
2025-07-10 16:07:39 +0800

    Delete index.py

--- index.py
+++
@@ -1,7 +0,0 @@
-from pyodide.http import open_url
-import csv
-import random
-
-reader = csv.reader(open_url("https://raw.githubusercontent.com/fromwindowstolinux/Python-Projects/main/Flask%20Project/manga.csv"))
-manga = random.choice(list(reader))        
-print ("Your manga recommendation is : ", manga[0])
